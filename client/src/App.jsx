f32cf632d69d69053844133feabf151f0b7257ff
Alireza Karimi <alirezayoom@gmail.com>
2023-10-20 23:38:53 +0430

    add react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
