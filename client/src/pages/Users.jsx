5cbb78460800b4b5bd78ed8eb6f8062bb9677887
Alireza Karimi <alirezayoom@gmail.com>
2023-10-21 11:02:51 +0430

    add pagination to users

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,36 +1,69 @@
 import { useState, useEffect } from "react";
 import classes from "./users.module.css";
+import { useSearchParams } from "react-router-dom";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams({ page: 1 });
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const data = await fetch("http://localhost:5000/api/Users");
+      const data = await fetch(
+        `http://localhost:5000/api/Users/?${searchParams}`
+      );
       const json = await data.json();
       setUsers(json);
     };
     fetchUsers();
-  }, []);
+  }, [searchParams, setSearchParams]);
 
   return (
-    <div className={classes.users}>
-      {users?.data?.map((user) => (
-        <div key={user._id} className={classes.user}>
-          <div className={classes.avatar}>
-            <img src={user.avatar} alt="avatar" />
+    <div>
+      <div className={classes.users}>
+        {users?.data?.map((user) => (
+          <div key={user._id} className={classes.user}>
+            <div className={classes.avatar}>
+              <img src={user.avatar} alt="avatar" />
+            </div>
+
+            <div className={classes.username}>@{user.username}</div>
+
+            <div className={classes.info}>
+              <div>{user.firstName}</div>
+              <div>{user.lastName}</div>
+            </div>
+
+            <div className={classes.email}>{user.email}</div>
           </div>
-
-          <div className={classes.username}>@{user.username}</div>
-
-          <div className={classes.info}>
-            <div>{user.firstName}</div>
-            <div>{user.lastName}</div>
-          </div>
-
-          <div className={classes.email}>{user.email}</div>
+        ))}
+      </div>
+      <div className={classes.pagination}>
+        <button onClick={() => setSearchParams({ page: 1 })}>first page</button>
+        <button
+          disabled={+searchParams.get("page") === 1}
+          onClick={() =>
+            setSearchParams({ page: +searchParams.get("page") - 1 })
+          }
+        >
+          prev
+        </button>
+        <div>
+          page {+users?.meta?.page} of {+users?.meta?.pageCount}
         </div>
-      ))}
+        <button
+          disabled={+searchParams.get("page") === users?.meta?.pageCount}
+          onClick={() =>
+            setSearchParams({ page: +searchParams.get("page") + 1 })
+          }
+        >
+          next
+        </button>
+        <button
+          onClick={() => setSearchParams({ page: +users?.meta?.pageCount })}
+        >
+          last page
+        </button>
+      </div>
     </div>
   );
 };
--- server/routers/Users.js
+++ server/routers/Users.js
@@ -4,8 +4,19 @@ import { UserModel } from "../models/Users.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const data = await UserModel.find({});
-  res.json({ data, message: "ok" });
+  const page = +req.query.page - 1 || 0;
+  const pageSize = 16;
+
+  const data = await UserModel.find()
+    .skip(page * pageSize)
+    .limit(pageSize);
+  res.json({
+    data,
+    meta: {
+      page: Number(page) + 1,
+      pageCount: Math.ceil((await UserModel.countDocuments()) / pageSize),
+    },
+  });
 });
 
 export { router as UserRouter };
