fb8e7a513a2b690bf250f2cf92291ebbe289fa46
Alireza Karimi <alirezayoom@gmail.com>
2023-10-21 07:00:07 +0430

    add dotenv

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import { UserRouter } from "./routers/Users.js";
 import dotenv from "dotenv";
+import { UserRouter } from "./routers/Users.js";
+import { PhotoRouter } from "./routers/Photos.js";
 
 const app = express();
 dotenv.config();
@@ -10,6 +11,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", UserRouter);
+app.use("/api/photos", PhotoRouter);
 
 mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection;
