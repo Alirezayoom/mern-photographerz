c7cb76ae1be5a9185b8fedfe559899bdb8209c90
Alireza Karimi <alirezayoom@gmail.com>
2023-10-21 07:00:59 +0430

    add users

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+import classes from "./users.module.css";
+
 const Users = () => {
-  <div>Users</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const data = await fetch("http://localhost:5000/api/Users");
+      const json = await data.json();
+      setUsers(json);
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className={classes.users}>
+      {users?.data?.map((user) => (
+        <div key={user._id} className={classes.user}>
+          <div className={classes.avatar}>
+            <img src={user.avatar} alt="avatar" />
+          </div>
+
+          <div className={classes.username}>@{user.username}</div>
+
+          <div className={classes.info}>
+            <div>{user.firstName}</div>
+            <div>{user.lastName}</div>
+          </div>
+
+          <div className={classes.email}>{user.email}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Users;
